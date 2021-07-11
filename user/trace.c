c97f92f6f92e43524d983525268d6d5ef3f2e640
williams4564 <williams4564@gmail.com>
2021-07-10 20:18:51 -0700

    Add files via upload

--- user/trace.c
+++ user/trace.c
@@ -6,20 +6,22 @@
 
 int main(int argc, char* argv[]){
 
-    if(strcmp(argv[0], "trace")){
+    if(strcmp(argv[0], "trace") == 0){
+        printf("Strcmp to trace success\n");
         int mask = atoi(argv[1]);
         if(mask < 0){
-            printf("Mask < 0, trace failed\n");
+            fprintf(2, "Mask < 0, trace failed\n");
             exit(1);
         }
-        int ret = trace(mask);
-        if(ret < 0)
-            printf("Return value <0, trace failed");
+        //int ret = trace(mask);
+        if(trace(mask) < 0)
+            fprintf(2, "Return value <0, trace failed");
         //copy arguments into another program
         char* array[MAXARG];
         int j;
         for(j = 2; j < argc; j++){
             array[j-2] = argv[j];
+            printf("array[%d] = %s\n", j-2, argv[j]);
         }
         exec(array[0], array);
     }
